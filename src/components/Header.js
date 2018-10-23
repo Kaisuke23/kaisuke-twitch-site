2c6b9586fb4350ec73a47f09912a0ddfc2ca0648
Kalani Sanidad <ksanidad@hawaii.edu>
2018-10-23 12:59:36 -0700

    added background and changed font sizes

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,6 +8,7 @@ const Header = (props) => (
         </div>
         <div className="content">
             <div className="inner">
+              <div className = "innerBackgroud">
                 <h1>Kaisuke</h1>
                 <p>If you ever think that you need to do things only one way, you are solemnly wrong. Always expect the unexpected and think outside the box.
                 </p>
@@ -15,14 +16,15 @@ const Header = (props) => (
                   <li><a href="https://twitter.com/kaisuke46/" className="icon fa-twitter"><span className="label">Twitter</span></a></li>
                   <li><a href="https://www.instagram.com/kaisuke_builds/" className="icon fa-instagram"><span className="label">Instagram</span></a></li>
                 </ul>
+              </div>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Stream</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Gundams</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('command')}}>Commands</a></li>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>F.A.Q</a></li>
+                <li ><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Stream</a></li>
+                <li ><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Gundams</a></li>
+                <li ><a href="javascript:;" onClick={() => {props.onOpenArticle('command')}}>Commands</a></li>
+                <li ><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>F.A.Q</a></li>
             </ul>
         </nav>
     </header>
